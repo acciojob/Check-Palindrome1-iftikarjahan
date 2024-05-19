3a77e1421ec4e6c87dadc6e06d9b9dc446d8dab1
iftikarjahan <69801895+iftikarjahan@users.noreply.github.com>
2024-05-19 10:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+	str=str.split(" ").join("");
+	let i=0;
+	let j=str.length-1;
+	let isPal=true;
+	while(i<j){
+		if(str[i]!==str[j]){
+			isPal=false;
+			break;
+		}
+		i++;
+		j--;
+	}
+	return isPal;
 }
 module.exports = palindrome
